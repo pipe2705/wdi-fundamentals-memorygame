7647199e426eb77e3f59a8a93e2388c7236ce1db
Felipe G <pipe2705@gmail.com>
2019-10-14 16:12:15 -0700

    created game Logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,18 +1,37 @@
 console.log("Up and running!");
 
-var cards = ["queen", "king", "queen", "king"];
+let cards = ["queen", "queen", "king", "king"];
 
-var cardsInPlay = [];
+let cardsInPlay = [];
+
+let cardOne = cards[0];
+
+cardsInPlay.push(cardOne);
+console.log( "User flipped" + " " + cardsInPlay)
+
+let cardTwo = cards[2];
+cardsInPlay.push(cardTwo);
+console.log( "User flipped" + " " + cardsInPlay[1]);
+
+//if (cardsInPlay.length === 2)
+//console.log(true);
+
+if (cardsInPlay[0] === cardsInPlay[1])
+
+alert("you found a match!");
+else alert("sorry try again");
+
+
 
 function checkForMatch() {
 
   if (cardsInPlay[0] === cardsInPlay[1])
-}
+
 console.log("You Found a Match");
-}  else {
+ else 
   console.log("Sorry, Try Again");
 }
-}
+
 
  function flipCard(cardId){
 flipCard(0);
